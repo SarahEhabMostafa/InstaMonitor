32b5f60410a6ed8ab103c1f88be2c4aea305052c
Sarah Ehab Mostafa <sarahehabm@gmail.com>
2016-05-23 19:03:10 +0200

    Initial commit

---
+++ app/src/main/java/com/sarahehabm/instamonitor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sarahehabm.instamonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
